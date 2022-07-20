2fc0d7741dc43407f85564c0665d34c81392ea11
Drashti Sharma <86773589+DrashtiSharma7983@users.noreply.github.com>
2022-07-20 13:15:44 +0530

    Add files via upload

---
+++ 2_set_theory.py
@@ -0,0 +1,13 @@
+p1 = int(input())
+p2 = int(input())
+p3 = int(input())
+q = int(input())
+e = int(input())
+r = int(input())
+first = 0
+second = 0
+second = e-r-p1-p2-p3+2*q
+x = int(second/3)
+first = x +p1+p3-q
+print(first)
+print(second)
